e427e9e96ef52483cb2139bc35c2f332e7a2c9db
Nguyen Minh Tuan <nguyen.minh.tuan@linecorp.com>
2018-12-05 15:41:08 +0700

    jwt token - don't store token anywhere

--- src/main/java/com/example/oauth1/config/ProviderServerConfig.java
+++ src/main/java/com/example/oauth1/config/ProviderServerConfig.java
@@ -16,14 +16,16 @@ import org.springframework.security.oauth2.provider.approval.ApprovalStore;
 import org.springframework.security.oauth2.provider.approval.JdbcApprovalStore;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
 public class ProviderServerConfig extends AuthorizationServerConfigurerAdapter {
 
-    @Autowired
-    DataSource dataSource;
+//    @Autowired
+//    DataSource dataSource;
 
     @Autowired
     AuthenticationManager authenticationManager;
@@ -36,41 +38,51 @@ public class ProviderServerConfig extends AuthorizationServerConfigurerAdapter {
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-//        clients.inMemory().withClient("chich").secret("chich")
-//               .scopes("app").authorizedGrantTypes("authorization_code")
-//               .redirectUris("http://localhost:9000/callback")
-//               .and().withClient("choe").secret("choe")
-//               .scopes("app").authorizedGrantTypes("implicit")
-//               .redirectUris("http://localhost:9000/callback")
-//               .accessTokenValiditySeconds(3600)
-//               .and().withClient("tac").secret("tac")
-//               .scopes("app").authorizedGrantTypes("password")
-//               .redirectUris("http://localhost:9000/callback")
-//               .and().withClient("ke").secret("ke")
-//               .authorizedGrantTypes("client_credentials", "password", "refresh_token")
-//               .accessTokenValiditySeconds(120)
-//               .refreshTokenValiditySeconds(300)
-//               .scopes("app");
-        clients.jdbc(dataSource);
+        clients.inMemory().withClient("chich").secret("chich")
+               .scopes("app").authorizedGrantTypes("authorization_code")
+               .redirectUris("http://localhost:9000/callback")
+               .and().withClient("choe").secret("choe")
+               .scopes("app").authorizedGrantTypes("implicit")
+               .redirectUris("http://localhost:9000/callback")
+               .accessTokenValiditySeconds(3600)
+               .and().withClient("tac").secret("tac")
+               .scopes("app").authorizedGrantTypes("password")
+               .redirectUris("http://localhost:9000/callback")
+               .and().withClient("ke").secret("ke")
+               .authorizedGrantTypes("client_credentials", "password", "refresh_token")
+               .accessTokenValiditySeconds(120)
+               .refreshTokenValiditySeconds(300)
+               .scopes("app");
+//        clients.jdbc(dataSource);
     }
 
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey("tu hu con");
+        return converter;
+    }
 
     @Bean
     public TokenStore tokenStore() {
 //        return new RedisTokenStore()
-        return new JdbcTokenStore(dataSource);
+//        return new JdbcTokenStore(dataSource);
+        return new JwtTokenStore(accessTokenConverter());
     }
 
-    @Bean
-    public ApprovalStore approvalStore() {
-        return new JdbcApprovalStore(dataSource);
-    }
+//    @Bean
+//    public ApprovalStore approvalStore() {
+//        return new JdbcApprovalStore(dataSource);
+//    }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         super.configure(endpoints);
         endpoints.authenticationManager(authenticationManager);
         endpoints.tokenStore(tokenStore());
-        endpoints.approvalStore(approvalStore());
+        endpoints.accessTokenConverter(accessTokenConverter());
+//        endpoints.tokenStore(tokenStore());
+//        endpoints.approvalStore(approvalStore());
     }
 }
