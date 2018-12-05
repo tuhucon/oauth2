b519a133e721bb7ae1c3a34ab2893bffaa698ce2
Nguyen Minh Tuan <nguyen.minh.tuan@linecorp.com>
2018-12-05 10:10:42 +0700

    store token + approval using jdbc

--- src/main/java/com/example/oauth1/config/ProviderServerConfig.java
+++ src/main/java/com/example/oauth1/config/ProviderServerConfig.java
@@ -1,6 +1,9 @@
 package com.example.oauth1.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.common.ExpiringOAuth2RefreshToken;
@@ -9,10 +12,17 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+import org.springframework.security.oauth2.provider.approval.JdbcApprovalStore;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
 public class ProviderServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
 
     @Autowired
     AuthenticationManager authenticationManager;
@@ -25,26 +35,40 @@ public class ProviderServerConfig extends AuthorizationServerConfigurerAdapter {
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory().withClient("chich").secret("chich")
-               .scopes("app").authorizedGrantTypes("authorization_code")
-               .redirectUris("http://localhost:9000/callback")
-               .and().withClient("choe").secret("choe")
-               .scopes("app").authorizedGrantTypes("implicit")
-               .redirectUris("http://localhost:9000/callback")
-               .accessTokenValiditySeconds(3600)
-               .and().withClient("tac").secret("tac")
-               .scopes("app").authorizedGrantTypes("password")
-               .redirectUris("http://localhost:9000/callback")
-               .and().withClient("ke").secret("ke")
-               .authorizedGrantTypes("client_credentials", "password", "refresh_token")
-               .accessTokenValiditySeconds(120)
-               .refreshTokenValiditySeconds(300)
-               .scopes("app");
+//        clients.inMemory().withClient("chich").secret("chich")
+//               .scopes("app").authorizedGrantTypes("authorization_code")
+//               .redirectUris("http://localhost:9000/callback")
+//               .and().withClient("choe").secret("choe")
+//               .scopes("app").authorizedGrantTypes("implicit")
+//               .redirectUris("http://localhost:9000/callback")
+//               .accessTokenValiditySeconds(3600)
+//               .and().withClient("tac").secret("tac")
+//               .scopes("app").authorizedGrantTypes("password")
+//               .redirectUris("http://localhost:9000/callback")
+//               .and().withClient("ke").secret("ke")
+//               .authorizedGrantTypes("client_credentials", "password", "refresh_token")
+//               .accessTokenValiditySeconds(120)
+//               .refreshTokenValiditySeconds(300)
+//               .scopes("app");
+        clients.jdbc(dataSource);
+    }
+
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JdbcTokenStore(dataSource);
+    }
+
+    @Bean
+    public ApprovalStore approvalStore() {
+        return new JdbcApprovalStore(dataSource);
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         super.configure(endpoints);
         endpoints.authenticationManager(authenticationManager);
+        endpoints.tokenStore(tokenStore());
+        endpoints.approvalStore(approvalStore());
     }
 }
--- src/main/java/com/example/oauth1/controller/UserController.java
+++ src/main/java/com/example/oauth1/controller/UserController.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.web.FilterChainProxy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
